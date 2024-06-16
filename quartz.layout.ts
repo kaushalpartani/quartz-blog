e4e22410af4c1d3767aed7d11862f008cd2120f1
Kaushal Partani <kaushalpartani@gmail.com>
2024-06-15 19:20:54 -0700

    Quartz sync: Jun 15, 2024, 7:20 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -39,13 +39,21 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [
+    Component.Breadcrumbs(), 
+    Component.ArticleTitle(), 
+    Component.ContentMeta()],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
-    Component.Darkmode(),
+    Component.MobileOnly(Component.Spacer()),
+    Component.MobileOnly(Component.Darkmode()),
     Component.DesktopOnly(Component.Explorer()),
+
   ],
-  right: [],
+  right: [
+    Component.DesktopOnly(Component.Darkmode()),
+    Component.MobileOnly(Component.Explorer()),
+  ],
 }
